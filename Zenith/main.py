2b2e8403ae56a03f387d7a08945c10ff37d74631
Zenreon <zenreon@gmail.com>
2021-09-18 21:56:01 -0500

    changed >>hello response, added time command and unfinished mathrand functions

--- Zenith/main.py
+++ Zenith/main.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 import os
 import random
 import asyncio
+import datetime, time
 
 
 # define bot prefix
@@ -22,21 +23,25 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def hello(ctx):
-        await ctx.send('Hello!')
+        await ctx.send('Hello, ' +format(ctx.author.mention) +'!')
 
 # >>help output
 
 @bot.command(pass_context=True)
 async def commands(ctx):
         await ctx.send("""I'm Zenith, a bot made for small-case uses. Written in python3 by Zenreon#3279. 
-**Prefix: >>
-``commands : Lists this text.
+**Prefix:** ``>>``
+``
+commands : Lists this text.
 hello : Responds "Hello!"
 mathadd num1 num2
 mathsub num1 num2
 mathmult num1 num2
 mathdiv num1 num2 (num1/num2)
-mathsqrt num1``
+mathsqrt num1
+mathrand num1 num2
+uptime : Displays total uptime
+time : Displays current CDT date and time``
         """)
 
 # mentions new user join
@@ -63,6 +68,8 @@ def sqrt(n: float):
 def mult(n: float, n2: float):
 	return n * n2
 
+def rand(n: int, n2: int):
+        return random.randint(n, n2)
 # end of math functions
 
 # bot commands for math functions
@@ -110,7 +117,46 @@ async def mathmult(ctx, x:float, y:float):
         except:
                 pass
 
+@bot.command(pass_context=True)
+async def mathrand(ctx, x:int, y:int):
+        try:
+                await ctx.send(x, y)
+        except:
+                pass
+
 # end of math commands
+
+# time output command
+
+@bot.command(pass_context=True)
+async def time(ctx):
+        now = datetime.datetime.now()
+        showtime = (now.strftime("Current CDT date and time: %Y-%m-%d, %H:%M:%S"))
+        try:
+                await ctx.send(showtime)
+        except:
+                pass
+
+# end of time command
+
+# uptime tracker and management commands begin
+
+@bot.command(pass_context=True)
+async def uptime(self, ctx):
+        current_time = time.time()
+        start_time = int(3)
+        difference = int(round(current_time - start_time))
+        text = str(datetime.timedelta(seconds=difference))
+        embed = discord.Embed(colour=0xc8dc6c)
+        embed.add_field(name='Uptime', value=text)
+        embed.set_footer(text=Zenith)
+        try:
+                await ctx.send(embed=embed)
+        except discord.HTTPException:
+                await ctx.send("Current Uptime: " + text)
+
+# end of uptime commands
+
 
 #bot token
 
