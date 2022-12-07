ca25a91a559605e31fe808d8ccb61652825cbd45
Zenreon <zenreon@gmail.com>
2022-12-07 12:06:19 -0600

    add UAC prompt

--- VRCEACKiller/Windows.bat
+++ VRCEACKiller/Windows.bat
@@ -1,33 +1,8 @@
 @echo off
-:: Require Admin
-:: BatchGotAdmin
-::-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-::--------------------------------------    
+:: Require Admin begin
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+:: Require admin End
 Set "VRC = VRchat.exe"
 echo %VRC%
 :: Start VRC When init from steam, sleep for EAC init
@@ -41,3 +16,4 @@ if "%ERRORLEVEL%"=="0" (taskkill /f /im EasyAntiCheat_EOS.exe)
 
 
 PAUSE
+exit
