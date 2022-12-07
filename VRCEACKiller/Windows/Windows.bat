cdd30decb2a41ac844e7a68232739c809ea2858b
Zenreon <zenreon@gmail.com>
2022-12-07 16:40:43 -0600

    add folder for elevated bat

---
+++ VRCEACKiller/Windows/Windows.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Get Admin
+::set "params=%*"
+::cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+:: Make Symbolic Link
+:: Start VRC When init from steam, sleep for EAC init
+"%~dp0"launch.exe
+ping -n 61 127.0.0.1 > nul
+:: Check if VRC is running
+tasklist /fi "ImageName eq VRChat.exe" /fo csv 2>NUL | find /I "VRChat.exe">NUL
+:: Wait 1m then kill EAC
+if "%ERRORLEVEL%"=="0" (ping -n 6 127.0.0.1 > nul) else (echo VRChat is not running.)
+if "%ERRORLEVEL%"=="0" (taskkill /f /im EasyAntiCheat_EOS.exe)
+
+
+PAUSE
+exit
---
+++ VRCEACKiller/Windows/elevated.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -command "Start-Process Windows.bat -Verb runas"
