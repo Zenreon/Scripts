436ad6dc193b433d1f5a34bfae6dc50ffa45baa1
Zenreon <zenreon@gmail.com>
2022-12-07 12:35:34 -0600

    add UAC

--- VRCEACKiller/Windows.bat
+++ VRCEACKiller/Windows.bat
@@ -3,8 +3,6 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 :: Get admin End
-Set "VRC = VRchat.exe"
-echo %VRC%
 :: Start VRC When init from steam, sleep for EAC init
 START start_protected_game1.exe
 ping -n 61 127.0.0.1 > nul
