08df1177b70a5cb8e9e2ea60f29cd5c20cd04794
Zenreon <zenreon@gmail.com>
2022-12-07 16:42:47 -0600

    add folder for elevated batch

--- VRCEACKiller/Windows/Windows.bat
+++ VRCEACKiller/Windows/Windows.bat
@@ -1,8 +1,4 @@
 @echo off
-:: Get Admin
-::set "params=%*"
-::cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-:: Make Symbolic Link
 :: Start VRC When init from steam, sleep for EAC init
 "%~dp0"launch.exe
 ping -n 61 127.0.0.1 > nul
