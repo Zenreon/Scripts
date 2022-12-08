907e1f7c0fe1f9c7c0283af80c31fec85f868763
Zenreon <zenreon@gmail.com>
2022-12-08 11:52:18 -0600

    comment change

--- VRCEACKiller/Windows/Windows.bat
+++ VRCEACKiller/Windows/Windows.bat
@@ -4,7 +4,7 @@
 ping -n 61 127.0.0.1 > nul
 :: Check if VRC is running
 tasklist /fi "ImageName eq VRChat.exe" /fo csv 2>NUL | find /I "VRChat.exe">NUL
-:: Wait 1m then kill EAC
+:: Wait 60sec then kill EAC
 if "%ERRORLEVEL%"=="0" (ping -n 6 127.0.0.1 > nul) else (echo VRChat is not running.)
 if "%ERRORLEVEL%"=="0" (taskkill /f /im EasyAntiCheat_EOS.exe)
 
